f1604544a149774997bed35f5244774f566ff5a6
Clint Hoagland <bachelor.one@gmail.com>
2023-05-27 14:43:08 -0400

    + Rider solution

---
+++ AdventOfCode2015/AdventOfCode2015/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode2015
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
